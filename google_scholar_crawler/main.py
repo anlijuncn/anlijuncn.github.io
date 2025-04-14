f390c6b4809580d4435b43d51f305ccf4e796ad4
An Lijun <anlijuncn@gmail.com>
2025-04-14 10:14:41 +0200

    Update main.py

--- google_scholar_crawler/main.py
+++ google_scholar_crawler/main.py
@@ -1,25 +1,23 @@
-from serpapi import GoogleSearch
 from scholarly import scholarly
+import jsonpickle
 import json
-import os
 from datetime import datetime
+import os
 
-
-author = scholarly.search_author_id(os.environ['GOOGLE_SCHOLAR_ID'])
-scholarly.fill(author, sections=['indices'])
-
-data = {
-    "name": author.get("name"),
-    "citedby": author.get("citedby"),
-    "updated": str(datetime.now())}
-
+author: dict = scholarly.search_author_id(os.environ['GOOGLE_SCHOLAR_ID'])
+scholarly.fill(author, sections=['basics', 'indices', 'counts', 'publications'])
+name = author['name']
+author['updated'] = str(datetime.now())
+author['publications'] = {v['author_pub_id']:v for v in author['publications']}
+print(json.dumps(author, indent=2))
 os.makedirs('results', exist_ok=True)
-with open('results/gs_data.json', 'w') as f:
-    json.dump(data, f, ensure_ascii=False, indent=2)
+with open(f'results/gs_data.json', 'w') as outfile:
+    json.dump(author, outfile, ensure_ascii=False)
 
-with open('results/gs_data_shieldsio.json', 'w') as f:
-    json.dump({
-        "schemaVersion": 1,
-        "label": "citations:",
-        "message": f"{data['citedby']}"
-    }, f, ensure_ascii=False)
+shieldio_data = {
+  "schemaVersion": 1,
+  "label": "citations:",
+  "message": f"{author['citedby']}",
+}
+with open(f'results/gs_data_shieldsio.json', 'w') as outfile:
+    json.dump(shieldio_data, outfile, ensure_ascii=False)
